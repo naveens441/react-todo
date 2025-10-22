8497ccb051d966349ebb2fd99dc3c531b7e42e31
Naveen <singhnaveen2105@gmail.com>
2025-10-22 14:08:36 +0530

    Refactor App component to initialize todos from local storage and improve formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 
 import { useEffect, useState } from "react";
-import { dummyData } from "./data/todos"
 import type { Todo } from "./types/todo";
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
 import TodoSummary from "./components/TodoSummary";
+import { dummyData } from "./data/todos";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>(dummyData);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]"
+    );
+    return savedTodos.length > 0 ? savedTodos : dummyData;
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
@@ -38,14 +42,14 @@ function App() {
 
   function deleteAllCompleted() {
     setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed));
-  } 
+  }
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Hello Neo!</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-9">
         <AddTodoForm onSubmit={addTodo} />
-        <TodoList todos={todos} onCompletedChange={setTodoCompleted} 
-        onDelete={deleteTodo}/>
+        <TodoList todos={todos} onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo} />
       </div>
       <TodoSummary
         todos={todos} deleteAllCompleted={deleteAllCompleted}
